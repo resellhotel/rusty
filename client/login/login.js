957c0379bb64c81d43ee543644f41043edd924b2
Matthew Delaney <mdelaney7@gmail.com>
2012-04-24 16:52:51 -0700

    Use $.serializeArray for login form instead.

--- client/login/login.js
+++ client/login/login.js
@@ -2,9 +2,9 @@ Template.loginForm.events = {
   'click #loginWithEmailButton': function (evt) {
     evt.preventDefault();
 
-    var inputs = $('#loginForm :input');
-    var userID = $(inputs[0]).val();
-    var userPassword = $(inputs[1]).val();
+    var form = $('#loginForm').serializeArray();
+    var userID = form[0]["value"];
+    var userPassword = form[1]["value"];
 
     App.login(userID, userPassword);
     // TODO: Pass in a callback to login to handle what happens on success/failure.
