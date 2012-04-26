47a187bcb03d7e0e39ab3ab4e3f033a5c517debb
Matthew Delaney <mdelaney7@gmail.com>
2012-04-26 01:46:43 -0700

    Super simple listings view under account.

--- client/account/account.js
+++ client/account/account.js
@@ -1,3 +1,13 @@
 Template.account.view_is = function (view) {
   return Session.equals("view", view);
 };
+
+Template.accountListings.listings = function () {
+  if (!App.isLoggedIn())
+    return [];
+
+  var userID = Session.get("userID");
+  var listingIDs = Users.findOne({_id: userID}).listings;
+  console.log(listingIDs);
+  return AnonListings.find({_id: { $in : listingIDs}});
+};
