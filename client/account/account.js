8d2f0c973952bce85f5edc2a5999cdb747b00f4d
Matthew Delaney <mdelaney7@gmail.com>
2012-05-11 12:09:21 -0700

    Moved account listing logic into listings.js

--- client/account/account.js
+++ client/account/account.js
@@ -1,21 +1,3 @@
 Template.account.view_is = function (view) {
   return Session.equals("view", view);
 };
-
-Template.accountListings.listings = function () {
-  if (!App.isLoggedIn())
-    return [];
-
-  var userID = Session.get("userID");
-  var listingIDs = Users.findOne({_id: userID}).listings;
-  return Listings.find({_id: { $in : listingIDs}});
-};
-
-Template.listingItem.events = {
-  'click .update-button' : function (e) {
-    Router.navigate("/account/listings/"+this._id, true);
-  },
-  'click .delete-button' : function (e) {
-    Listings.remove({_id: this._id});
-  }
-};
