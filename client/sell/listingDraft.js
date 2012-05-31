e7c38ea7a6126cdaaa8af5c798dc82878a49dbec
Matthew Delaney <mdelaney7@gmail.com>
2012-05-30 22:03:20 -0700

    Update Listing Draft to use new way to init date picker.

--- client/sell/listingDraft.js
+++ client/sell/listingDraft.js
@@ -1,43 +1,45 @@
 Template.reservationInfoForm.init = function () {
-  Meteor.flush();
-  // TODO: Figure out why this timeout and fluhses are needed, remove those hacks.
+  initDatePicker('checkinDate', '07/07/2012');
+  initDatePicker('checkoutDate', '07/08/2012');
 
-  Meteor.setTimeout(function () {
-    Meteor.flush();
-    var listingID = Session.equals("mode", "sell") ? Session.get('listingDraftID') : Session.get('listingID');
-    var listing = Listings.findOne({_id: listingID});
+  var listingID = Session.equals("mode", "sell") ? Session.get('listingDraftID') : Session.get('listingID');
+  var listing = Listings.findOne({_id: listingID});
 
-    // Init Inputs
-    var ids = ['checkinDate', 'checkoutDate', 'confirmationNumber', 'confirmationSource', 'hotelName', 'hotelCity', 'hotelState', 'price'];
-    _.each(ids, function (id) {
-      var sel = '#'+id;
-      var startVal = listing[id];
-      $(sel).val(startVal);
-      $(sel).click(function (e) {
-        e.preventDefault();
-        $(sel+'Picker').datepicker('show');
-      });
+  if (!listing)
+    return;
 
-      var dp = $(sel+'Picker');
-      if (dp) {
-        dp.data('date', startVal);
-
-        $(sel+'Picker').datepicker({ format: 'mm/dd/yyyy'}).on('changeDate', function (evt){
-          // TODO: Validate the change.
-
-          // Update the listing draft
-          var o = {};
-          o[id] = $(sel).val();
-          Listings.update({_id: listingID}, {$set: o});
-
-          // Dismiss the picker
-          $(sel+'Picker').datepicker('hide');
-        }); //onchange
-      }
-    });
-
-  }, 500);
+  // Init Inputs
+  var ids = ['confirmationNumber', 'confirmationSource', 'hotelName', 'hotelCity', 'hotelState', 'price'];
+  _.each(ids, function (id) {
+    var startVal = listing[id];
+    var sel = '#'+id;
+    $(sel).val(startVal);
+  });
 };
+
+  // var dp = $(sel+'Picker');
+
+  // if (dp && dp.length) {
+  //   $(sel).click(function (e) {
+  //     e.preventDefault();
+  //     $(sel+'Picker').datepicker('show');
+  //   });
+
+  //   $(sel).val(startVal);
+  //   dp.data('date', startVal);
+
+  //   $(sel+'Picker').datepicker({ format: 'mm/dd/yyyy'}).on('changeDate', function (evt){
+  //     // TODO: Validate the change.
+
+  //     // Update the listing draft
+  //     var o = {};
+  //     o[id] = $(sel).val();
+  //     Listings.update({_id: listingID}, {$set: o});
+
+  //     // Dismiss the picker
+  //     $(sel+'Picker').datepicker('hide');
+  //   }); //onchange
+  // } else {
 
 Template.listingDraft.mode_is = function (mode) {
   return Session.equals("mode", mode);
